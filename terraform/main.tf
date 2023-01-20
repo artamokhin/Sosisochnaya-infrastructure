db10023c375810d81580b011aa2e549994c60b0f
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-01-20 20:33:22 +0100

    terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,15 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-
-  # Описание бэкенда хранения состояния
-  backend "s3" {
-    endpoint = "storage.yandexcloud.net"
-    bucket   = "terraform-state-aleksandr-artamokhin"
-    region   = "ru-central1"
-    key      = "terraform.tfstate"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
-}
-
-provider "yandex" {
-  cloud_id  = "b1g3jddf4nv5e9okle7p"
-  folder_id = "b1g6kb8sqccdk2sg5drr"
-  zone      = "ru-central1-a"
-}
+module "tf-yc-instance" {
+  source = "./modules/tf-yc-instance"
+#  image_id = var.image_id
+#  zone = var.zone
+#  subnet_id = [
+#    for k, subnet in module.tf-yc-network.yandex_vpc_subnets :
+#    subnet if subnet.zone == var.instance_zone
+#  ][0].subnet_id
+} 
 
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "chapter5-lesson2-std-012-056"
 
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd80qm01ah03dkqb14lc"
-    }
-  }
-
-  network_interface {
-    subnet_id = "e9bglr3c0r3cklccie5m"
-    nat       = false
-  }
-
-  metadata = {
-   user-data = "${file("/home/student/example-01/meta.txt")}"
-  }
-}
-
-
-output "ip_address" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
+module "tf-yc-network" {
+  source = "./modules/tf-yc-network"
+} 
---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "cloud_id" {
+  default     = "b1g3jddf4nv5e9okle7p"
+  type        = string
+  description = "Yandex Cloud ID"
+  sensitive = true
+  nullable = false
+} 
+
+variable "folder_id" {
+  default     = "b1g6kb8sqccdk2sg5drr"
+  type        = string
+  description = "YandexCloud folder ID"
+  sensitive = true
+  nullable = false
+}
+
+variable "zone" {
+  default     = "ru-central1-a"
+  type        = string
+  description = "Instance availability zone"
+  sensitive = true
+  nullable = false
+}
+
+variable "subnet_id" {
+  default     = "e9bglr3c0r3cklccie5m"
+  type        = string
+  description = "Subnet ID"
+  sensitive = false
+  nullable = false
+}
