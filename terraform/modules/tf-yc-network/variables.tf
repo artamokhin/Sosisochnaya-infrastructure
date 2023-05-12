559c2e445a3697d7c79e9fe256f6e8cbd84eef60
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-05-12 16:08:42 +0200

    Add grafana dashboard config

--- terraform/modules/tf-yc-network/variables.tf
+++ terraform/modules/tf-yc-network/variables.tf
@@ -1,39 +1,39 @@
 variable "network_zones" {
-  default     = (["ru-central1-a","ru-central1-b","ru-central1-c"])
+  default     = (["ru-central1-a", "ru-central1-b", "ru-central1-c"])
   type        = set(string)
   description = "YandexCloud network zones list"
-  sensitive = false
-  nullable = false
+  sensitive   = false
+  nullable    = false
 }
 
 variable "zone" {
   default     = "ru-central1-a"
   type        = string
   description = "Instance availability zone"
-  sensitive = true
-  nullable = false
+  sensitive   = true
+  nullable    = false
 }
 
 variable "subnet_id" {
   default     = "e9bglr3c0r3cklccie5m"
   type        = string
   description = "Subnet ID"
-  sensitive = false
-  nullable = false
+  sensitive   = false
+  nullable    = false
 }
 
 variable "folder_id" {
   default     = "b1g6kb8sqccdk2sg5drr"
   type        = string
   description = "YandexCloud folder ID"
-  sensitive = true
-  nullable = false
+  sensitive   = true
+  nullable    = false
 }
 
 variable "cloud_id" {
   default     = "b1g3jddf4nv5e9okle7p"
   type        = string
   description = "Yandex Cloud ID"
-  sensitive = true
-  nullable = false
-} 
+  sensitive   = true
+  nullable    = false
+}
